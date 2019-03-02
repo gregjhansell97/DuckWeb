9e25d6d3d7c01f1e4306d0c04d0a11526ad9d06a
Arthur He <arthurhe8@gmail.com>
2019-03-02 16:03:55 -0500

    moved files

--- src/demos/artgallery/src/components/Header.js
+++ src/demos/artgallery/src/components/Header.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import banner from './images/starrynight.png';
+import banner from '../images/starrynight.png';
 
 export default class Header extends Component {
   render() {
     return (
       <div className="row">
         <div className="banner">
-          <img src={banner} width="100%" height={300}/>
+            <img src={banner} width="100%" height={300}/>
         </div>
       </div>
     );
