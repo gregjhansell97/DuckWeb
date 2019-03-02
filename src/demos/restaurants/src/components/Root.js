5c5eba90d0e85ee790fe8dcbf410e4d7e04bb29d
gregjhansell97 <gregjhansell@gmail.com>
2019-03-02 15:55:40 -0500

    restaurants initial setup page

---
+++ src/demos/restaurants/src/components/Root.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import {withStyles} from "@material-ui/core/styles";
+
+class Root extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world</h1>
+      	    </div>
+    	);
+    }
+}
+
+export default Root;
