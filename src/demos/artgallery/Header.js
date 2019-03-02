f17f86c177858eb31c3d8a1cb541d4d8359c51f5
Arthur He <arthurhe8@gmail.com>
2019-03-02 15:07:41 -0500

    adding artgallery folder

---
+++ src/demos/artgallery/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import banner from './images/starrynight.png';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="banner">
+          <img src={banner} width="100%" height={300}/>
+        </div>
+      </div>
+    );
+  }
+}
